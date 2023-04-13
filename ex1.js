230d3078515fe158b656413003d9e89db0737c34
taherehfazeli <tahereh1990@gmail.com>
2023-04-13 22:10:08 +0330

    question1answer

---
+++ ex1.js
@@ -0,0 +1,35 @@
+
+// Exercise 1.1 - Variables
+// Create a variable called x, assign the value 50 to it.
+// Answer:
+
+
+
+
+
+// Exercise 1.2 - Operators
+// Create another variable called y set its value to x times 100.
+// Answer:
+
+
+
+
+
+// Exercise 1.3 - Data Types
+// Describe the correct data type of the following variables:
+
+let width = 30              //Answer:
+let height = 40.2           //Answer:
+let title = "Hello"         //Answer:
+const person = {            //Answer:
+    firstName: "Ali",
+    lastName: "Akbari"
+}
+
+
+
+
+
+// exercise 1.4 - Functions
+// Create a function called "myFunction" that returns "Hello!".
+// Answer:
