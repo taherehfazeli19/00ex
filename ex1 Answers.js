d9129931ea82e91aaab4c182472dd896d9d857e8
taherehfazeli <tahereh1990@gmail.com>
2023-04-13 22:37:43 +0330

    question1eddit

--- ex1 Answers.js
+++ ex1 Answers.js
@@ -11,25 +11,25 @@ var x=50
 // Create another variable called y set its value to x times 100.
 // Answer:
 
-
-
+var y=x*100
 
 
 // Exercise 1.3 - Data Types
 // Describe the correct data type of the following variables:
 
-let width = 30              //Answer:
-let height = 40.2           //Answer:
-let title = "Hello"         //Answer:
-const person = {            //Answer:
+let width = 30              //Answer: Integer
+let height = 40.2           //Answer: Float
+let title = "Hello"         //Answer:STTRING
+const person = {            //Answer:Constant Array
     firstName: "Ali",
     lastName: "Akbari"
 }
 
 
 
-
-
 // exercise 1.4 - Functions
 // Create a function called "myFunction" that returns "Hello!".
 // Answer:
+function myFunction() {
+    return "Hello!"
+  }
